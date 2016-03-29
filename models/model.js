570c73a0b686ca270ca8d76a8fadf667e21f393d
kostuyn <kostuyn@gmail.com>
2016-03-29 19:31:45 +0300

    update

--- models/model.js
+++ models/model.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var schema = mongoose.Schema({
     field: String,
-    created: Date
+    created: {type: Date, default: moment.utc}
 });
 
 module.exports = mongoose.model('myModel', schema);
